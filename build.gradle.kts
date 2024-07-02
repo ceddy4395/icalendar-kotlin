d758aceff987996e260ff20592cb11ca5d516d7f
Cedric Willekens <cedric@willekens.dev>
2024-07-02 14:28:47 +0200

    Bump versions and add auto release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,25 +4,44 @@ group = "com.vituary"
 version = "0.1.0"
 
 plugins {
-    kotlin("jvm") version "1.3.50"
-    maven
+    kotlin("jvm") version "1.8.20"
+//    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("maven-publish")
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 repositories {
     mavenLocal()
-    jcenter()
+    mavenCentral()
 }
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
         freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ceddy4395/icalendar-kotlin")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
 }
